dbff6e5cb719bb830eb99c9c5029734c164a57fe
Aral Sarrafi <33207285+Aral-Sarrafi@users.noreply.github.com>
2018-05-17 20:34:10 -0400

    Add files via upload

--- My_Functions.py
+++ My_Functions.py
@@ -11,197 +11,7 @@ import os
 import matplotlib.pyplot as plt
 
 
-# In[2]:
-
-
-# Camera Calibration
-calib_file = 'camera_cal/'
-calib_img = os.listdir(calib_file)
-
-nx = 9
-ny = 6
-
-image_points = []
-object_points = []
-
-objp = np.zeros((nx*ny,3), np.float32)
-objp[:,:2] = np.mgrid[0:nx,0:ny].T.reshape(-1,2)
-
-
-for image_name in calib_img:
-    
-    image = cv2.imread(calib_file + image_name)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    
-    ret, corners = cv2.findChessboardCorners(gray, (nx, ny), None)
-    if ret == True:
-        image_points.append(corners)
-        object_points.append(objp)
-
-
-# In[3]:
-
-
-ret, mtx, dist, rvecs, tvects = cv2.calibrateCamera(object_points, image_points, gray.shape, None, None)
-
-
-# In[4]:
-
-
-# undistort two example images:
-img = cv2.imread(calib_file + calib_img[1])
-dst = cv2.undistort(img, mtx, dist,None, mtx)
-
-fig, axs  = plt.subplots(1,2, figsize = (10,4))
-axs = axs.ravel()
-
-
-axs[0].imshow(dst)
-axs[0].set_title('corrected image')
-axs[1].imshow(img)
-axs[1].set_title('original image')
-
-
-# In[435]:
-
-
-# Select one of the images from the test images folder to start implementing advanced lane finding method
-test_images_file = "test_images/"
-test_images = os.listdir(test_images_file)
-
-image_name = test_images_file + test_images[7]
-image = cv2.imread(image_name)
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-
-# In[436]:
-
-
-# Undistord the selected test image
-undist_image = cv2.undistort(image, mtx, dist, None, mtx)
-
-
-# In[437]:
-
-
-fig, axs  = plt.subplots(1,2, figsize = (10,4))
-axs = axs.ravel()
-
-
-axs[0].imshow(undist_image)
-axs[0].set_title('corrected image')
-axs[1].imshow(image)
-axs[1].set_title('original image')
-
-
-# In[438]:
-
-
-# Color Threshold on HLS(Hue, Lightness, saturation) images 
-HLS_img = cv2.cvtColor(undist_image, cv2.COLOR_BGR2HLS)
-
-H_channel = HLS_img[:,:,0]
-L_channel = HLS_img[:,:,1]
-S_channel = HLS_img[:,:,2]
-
-
-# In[439]:
-
-
-fig, axs = plt.subplots(1,3, figsize = (15,6))
-axs = axs.ravel()
-
-axs[0].imshow(H_channel, cmap = 'gray')
-axs[0].set_title('H Channel')
-
-axs[1].imshow(L_channel, cmap = 'gray')
-axs[1].set_title("L Channel")
-
-axs[2].imshow(S_channel, cmap = 'gray')
-axs[2].set_title('S Channel')
-
-
-# In[440]:
-
-
-# Threshold the S channel
-thresh = (100, 255)
-S_binary = np.zeros_like(S_channel)
-S_binary[(S_channel>thresh[0]) & (S_channel<=thresh[1])] = 1
-
-plt.imshow(S_binary, cmap = 'gray')
-
-
-# In[441]:
-
-
-# Gradiend Threshold
-gray_img = cv2.cvtColor(undist_image, cv2.COLOR_BGR2GRAY)
-
-grad_x = cv2.Sobel(gray_img, cv2.CV_64F, 1, 0)
-grad_y = cv2.Sobel(gray_img, cv2.CV_64F, 0, 1)
-
-grad_mag = np.sqrt(grad_x**2 + grad_y**2)
-grad_dir = np.arctan2(np.abs(grad_y),np.abs(grad_x))
-
-fig, axs = plt.subplots(2,2, figsize = (15,6))
-axs = axs.ravel()
-
-axs[0].imshow(grad_x, cmap = 'gray')
-axs[0]. set_title("grad x")
-
-axs[1].imshow(grad_y, cmap = 'gray')
-axs[1]. set_title("grad y")
-
-
-axs[2].imshow(grad_mag, cmap = 'gray')
-axs[2]. set_title("grad mag")
-
-axs[3].imshow(grad_dir, cmap = 'gray')
-axs[3]. set_title("grad dir")
-
-
-# In[442]:
-
-
-# Threshold the grad x
-thresh = (90, 255)
-binary_gradx = np.zeros_like(grad_x)
-binary_gradx[(grad_x>= thresh[0]) & (grad_x<= thresh[1])] = 1
-
-plt.imshow(binary_gradx, cmap= 'gray')
-
-
-# In[443]:
-
-
-# Combine the Color Threshold and the gradient threshold
-binary_img = np.zeros_like(S_binary)
-
-binary_img[(S_binary ==1) | (binary_gradx ==1)] = 1
-
-plt.imshow(binary_img)
-
-
-# In[444]:
-
-
-# Use perespective transform to obtain a birds eye view of the road
-
-
-
-plt.imshow(undist_image)
-plt.plot(250, 680,'.')
-plt.plot(1030, 680,'.')
-plt.plot(500, 510,'.')
-plt.plot(770,510,'.')
-plt.show()
-
-
-print(undist_image.shape)
-
-
-# In[445]:
+# In[1]:
 
 
 def warp(img):
@@ -218,17 +28,11 @@ def warp(img):
      [325, 500],
      [990, 500]])
     
-
-    
     M = cv2.getPerspectiveTransform(src,dst)
     Minv = cv2.getPerspectiveTransform(dst,src)
     warped = cv2.warpPerspective(img, M, img_size, flags = cv2.INTER_LINEAR)
     
     return warped, Minv
-
-bird_eye, Minv = warp(undist_image)
-
-plt.imshow(bird_eye)
 
 
 # In[446]:
@@ -332,28 +136,19 @@ def combine_thresholds(binary_1, binary_2):
 # In[452]:
 
 
-def Generate_Binary_Warped(img):
+def Generate_Binary_Warped(img, mtx, dist):
     
-    corrected_img = cv2.undistort(img, mtx, dist, None, mtx)
+    undist_image = cv2.undistort(img, mtx, dist, None, mtx)
     
-    Color_binary = HLS_threshold(corrected_img, (200, 255))
-    Grad_binary = grad_thresh(corrected_img, (30, 255), (200, 255), (60, 255), (0.35, 1.57))
+    Color_binary = HLS_threshold(undist_image, (210, 255))
+    Grad_binary = grad_thresh(undist_image, (30, 255), (200, 255), (60, 255), (0.35, 1.57))
     
     combined_binary = combine_thresholds(Color_binary, Grad_binary)
     
-    result, Minv = warp(combined_binary)
+    binary_warped, Minv = warp(combined_binary)
   
     
-    return result
-
-
-# In[453]:
-
-
-my_image = cv2.imread(image_name)
-binary_warped = Generate_Binary_Warped(my_image)
-
-plt.imshow(binary_warped, cmap = 'gray')
+    return binary_warped, Minv, undist_image
 
 
 # In[454]:
@@ -435,50 +230,89 @@ def Image_Lane_detection(binary_warped):
     return left_fit, right_fit, leftx, lefty, rightx, righty
 
 
-# In[455]:
+# In[3]:
 
 
-left_fit, right_fit, leftx, lefty, rightx, righty = Image_Lane_detection(binary_warped)
+def annotate_image(undist_image, binary_warped, Minv, left_fit, right_fit):
+
+    ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
+    left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+    right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+    
+    # Create an image to draw the lines on
+    warp_zero = np.zeros_like(binary_warped).astype(np.uint8)
+    color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
+
+    # Recast the x and y points into usable format for cv2.fillPoly()
+    pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+    pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
+    pts = np.hstack((pts_left, pts_right))
+
+    # Draw the lane onto the warped blank image
+    cv2.fillPoly(color_warp, np.int_([pts]), (0,255, 0))
+
+    # Warp the blank back to original image space using inverse perspective matrix (Minv)
+    newwarp = cv2.warpPerspective(color_warp, Minv, (undist_image.shape[1], undist_image.shape[0])) 
+    # Combine the result with the original image
+    result = cv2.addWeighted(undist_image, 1, newwarp, 0.3, 0)
+    
+    return result
 
 
-# In[456]:
+# In[4]:
 
 
-# Generate x and y values for plotting
-out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
-ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
-left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
-right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
-
-out_img[lefty, leftx] = [255, 0, 0]
-out_img[righty, rightx] = [0, 0, 255]
-plt.imshow(out_img)
-plt.plot(left_fitx, ploty, color='yellow')
-plt.plot(right_fitx, ploty, color='yellow')
-plt.xlim(0, 1280)
-plt.ylim(720, 0)
+def IMAGE_PROCESS(img, mtx, dist):
+    
+    binary_warped, Minv, undist_image = Generate_Binary_Warped(img, mtx, dist)
+    
+    left_fit, right_fit, leftx, lefty, rightx, righty = Image_Lane_detection(binary_warped)
+    
+    output = annotate_image(undist_image, binary_warped, Minv, left_fit, right_fit)
+    
+    return output, left_fit, right_fit  
 
 
-# In[457]:
+# In[ ]:
 
 
-# Create an image to draw the lines on
-warp_zero = np.zeros_like(binary_warped).astype(np.uint8)
-color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
+def VIDEO_PROCESS(cap, mtx, dist, Video_name):
+    counter = 0
+    
+    # Video infomration    
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    frameCount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    frameWidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frameHight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    
+    # output video spec
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter(Video_name, fourcc, fps,(frameWidth,frameHight))
 
-# Recast the x and y points into usable format for cv2.fillPoly()
-pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
-pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
-pts = np.hstack((pts_left, pts_right))
+    for i in range(50):
+        
+        ret, img = cap.read()
+        
+        if ret == True:
+            if counter == 0:
 
-# Draw the lane onto the warped blank image
-cv2.fillPoly(color_warp, np.int_([pts]), (0,255, 0))
+                annotated_img, left_fit, right_fit = IMAGE_PROCESS(img, mtx, dist)
+                annotated_img = cv2.cvtColor(annotated_img, cv2.COLOR_BGR2RGB)
+                out.write(annotated_img)
 
-# Warp the blank back to original image space using inverse perspective matrix (Minv)
-newwarp = cv2.warpPerspective(color_warp, Minv, (image.shape[1], image.shape[0])) 
-# Combine the result with the original image
-result = cv2.addWeighted(undist_image, 1, newwarp, 0.3, 0)
-plt.imshow(result)
+            else:
+
+                binary_warped, Minv, undist_image = Generate_Binary_Warped(img, mtx, dist)
+
+                left_fit, right_fit, leftx, lefty, rightx, righty = Frame_Lane_detection(binary_warped, left_fit , right_fit)
+
+                annotated_img = annotate_image(undist_image, binary_warped, Minv, left_fit, right_fit)
+                annotated_img = cv2.cvtColor(annotated_img, cv2.COLOR_BGR2RGB)
+        else:
+            break
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
 
 
 # In[458]:
@@ -516,46 +350,6 @@ def Frame_Lane_detection(binary_warped, left_fit , right_fit):
     return left_fit, right_fit, leftx, lefty, rightx, righty
 
 
-# In[459]:
-
-
-left_fit, right_fit, leftx, lefty, rightx, righty = Frame_Lane_detection(binary_warped, left_fit, right_fit)
-
-
-# In[460]:
-
-
-# Create an image to draw on and an image to show the selection window
-out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
-window_img = np.zeros_like(out_img)
-# Color in left and right line pixels
-out_img[lefty, leftx] = [255, 0, 0]
-out_img[righty, rightx] = [0, 0, 255]
-
-# Generate a polygon to illustrate the search window area
-# And recast the x and y points into usable format for cv2.fillPoly()
-margin = 100
-
-left_line_window1 = np.array([np.transpose(np.vstack([left_fitx-margin, ploty]))])
-left_line_window2 = np.array([np.flipud(np.transpose(np.vstack([left_fitx+margin, 
-                              ploty])))])
-left_line_pts = np.hstack((left_line_window1, left_line_window2))
-right_line_window1 = np.array([np.transpose(np.vstack([right_fitx-margin, ploty]))])
-right_line_window2 = np.array([np.flipud(np.transpose(np.vstack([right_fitx+margin, 
-                              ploty])))])
-right_line_pts = np.hstack((right_line_window1, right_line_window2))
-
-# Draw the lane onto the warped blank image
-cv2.fillPoly(window_img, np.int_([left_line_pts]), (0,255, 0))
-cv2.fillPoly(window_img, np.int_([right_line_pts]), (0,255, 0))
-result = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
-plt.imshow(result)
-plt.plot(left_fitx, ploty, color='yellow')
-plt.plot(right_fitx, ploty, color='yellow')
-plt.xlim(0, 1280)
-plt.ylim(720, 0)
-
-
 # In[461]:
 
 
@@ -578,12 +372,4 @@ def Lane_Curveture(leftx, lefty, rightx, righty):
     right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
 
     return left_curverad, right_curverad
-
-
-# In[462]:
-
-
-left_curverad, right_curverad = Lane_Curveture(leftx, lefty, rightx, righty)
-
-print(left_curverad, 'm', right_curverad, 'm')
 
